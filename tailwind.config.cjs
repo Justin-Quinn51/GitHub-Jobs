0212366b66cbcad400888ca9109b152b958225cd
Justin Quinn <jpq3366@gmail.com>
2022-08-23 20:09:40 -0400

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{react,js,jsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
