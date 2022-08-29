8a63b452bb5a74fc95723a13ab36340b2cb60816
Justin Quinn <jpq3366@gmail.com>
2022-08-29 12:46:39 -0400

    Working on desktop layout

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -2,7 +2,7 @@
 export default function Tile({logo, company, newPost, featured, position, postedAt, contract, location, role, level, tools, languages, key}) {
     
     return (
-        <div className={ featured ? 'bg-white w-4/5 pb-2 mx-auto shadow-lg border-l-8 border-l-aqua': 'bg-white w-4/5 pb-2 mx-auto shadow-lg' }>
+        <div className={ featured ? 'bg-white w-4/5 pb-2 mx-auto shadow-lg border-l-8 border-l-aqua my-6 md:flex' : 'bg-white w-4/5 pb-2 mx-auto shadow-lg md:flex my-6' }>
           <section className='ml-6 mt-12 leading-8'>
             <img className='mb-2' src={logo} alt="Company Logo" />
             <span className="text-aqua mr-6">{company}</span>
@@ -11,12 +11,12 @@ export default function Tile({logo, company, newPost, featured, position, posted
             <h2 className='font-bold'>{position}</h2>
             <span className="text-light-gray">{postedAt} <span className=''>&middot;</span> {contract} <span className=''>&middot;</span> {location}</span>
           </section>
-          <hr className='mt-3 underline'/>
-          <section className='flex flex-wrap gap-3 ml-6 my-3 mx-2'>
+          <hr className='mt-3 underline lg:hidden'/>
+          <section className='flex flex-wrap gap-3 ml-6 my-3 mx-2 lg:ml-auto pr-20'>
             <div className='bg-zinc-200 p-1.5 text-aqua'>{role}</div>
             <div className='bg-zinc-200 p-1.5 text-aqua'>{level}</div>
-            { languages.map((language) => <div key={languages.id} className='bg-zinc-200 p-1.5 text-aqua'>{language}</div>) }
-            { tools.map((tool) => <div key={tools.id} className='bg-zinc-200 p-1.5 text-aqua'>{tool}</div>) }
+            { languages.map((language) => <div key={key} className='bg-zinc-200 p-1.5 text-aqua'>{language}</div>) }
+            { tools.map((tool) => <div key={key} className='bg-zinc-200 p-1.5 text-aqua'>{tool}</div>) }
           </section>
         </div>
     )
