1a649ea0bcceea67d358b337c6d6594e9517f7c1
Justin Quinn <jpq3366@gmail.com>
2022-08-30 06:43:44 -0400

    Desktop layout mostly complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 export default function Header() {
     return (
         <div className=''>
-            <img className='w-full h-56' src="/bg-header-mobile.svg" alt="Background Header Mobile" />
+            <img className='w-full h-48' src="/bg-header-mobile.svg" alt="Background Header Mobile" />
         </div>
     )
 }
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -2,19 +2,19 @@
 export default function Tile({logo, company, newPost, featured, position, postedAt, contract, location, role, level, tools, languages, key}) {
     
     return (
-        <div className={ featured ? 'bg-white w-4/5 pb-2 mx-auto shadow-lg border-l-8 border-l-aqua my-6 lg:flex' : 'bg-white w-4/5 pb-2 mx-auto shadow-lg my-6 lg:flex' }>
-          <section className='mt-12 leading-8 lg:flex'>
-              <img className='ml-6 mb-2' src={logo} alt="Company Logo" />
-            <div className='ml-6 lg:w-72'>
-              <span className="text-aqua mr-6">{company}</span>
-              { newPost === true && (<button className='text-white font-bold bg-aqua-dark rounded-full p-1.5 mr-2'>NEW!</button>) }
-              { featured === true && (<button className='text-white font-bold bg-dark-green rounded-full p-1.5'>FEATURED</button>) }
-              <h2 className='font-bold hover:text-aqua cursor-pointer'>{position}</h2>
-              <span className="text-light-gray">{postedAt} <span className=''>&middot;</span> {contract} <span className=''>&middot;</span> {location}</span>
+        <div className={ featured ? 'bg-white w-5/6 pb-1 mx-auto shadow-lg border-l-4 border-l-aqua my-6 lg:flex py-3' : 'bg-white w-5/6 pb-2 mx-auto shadow-lg my-6 lg:flex py-3' }>
+          <section className='lg:flex items-center'>
+              <img className='ml-6 mb-2 lg:mt-2' src={logo} alt="Company Logo" />
+            <div className='ml-6 lg:'>
+              <span className="text-aqua font-bold mr-6 lg:mr-3">{company}</span>
+              { newPost === true && (<button className='text-white font-bold bg-aqua-dark rounded-full px-2 py-0.5 mr-2'>NEW!</button>) }
+              { featured === true && (<button className='text-white font-bold bg-dark-green rounded-full px-2 py-0.5'>FEATURED</button>) }
+              <h2 className='font-bold hover:text-aqua cursor-pointer lg:text-lg'>{position}</h2>
+              <span className="text-light-gray lg:flex gap-4">{postedAt} <span className=''>&middot;</span>   {contract} <span className=''>&middot;</span> {location}</span>
             </div>
           </section>
           <hr className='mt-3 underline lg:hidden'/>
-          <section className='flex flex-wrap gap-3 ml-6 my-3 mx-2 lg:ml-auto pr-20 items-center'>
+          <section className='flex flex-wrap gap-3 ml-6 my-3 lg:ml-auto pr-20 items-center'>
             <div className='bg-zinc-200 p-1.5 text-aqua hover:bg-aqua cursor-pointer hover:text-white'>{role}</div>
             <div className='bg-zinc-200 p-1.5 text-aqua hover:bg-aqua cursor-pointer hover:text-white'>{level}</div>
             { languages.map((language) => <div key={key} className='bg-zinc-200 p-1.5 text-aqua hover:bg-aqua cursor-pointer hover:text-white'>{language}</div>) }
